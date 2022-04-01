e2f122fb582d58742d587ddc9490052676e443cf
Kerry Archibald <kerrya@element.io>
2022-04-01 14:56:24 +0200

    basic wiring
    Signed-off-by: Kerry Archibald <kerrya@element.io>

--- src/sync.ts
+++ src/sync.ts
@@ -1641,6 +1641,8 @@ export class SyncApi {
         // to be decorated with sender etc.
         const [mainTimelineEvents, threadedEvents] = this.client.partitionThreadedEvents(room, timelineEventList || []);
         room.addLiveEvents(mainTimelineEvents, null, fromCache);
+        this.client.processBeaconEvents(room, timelineEventList);
+
         await this.processThreadEvents(room, threadedEvents, false);
     }
 
